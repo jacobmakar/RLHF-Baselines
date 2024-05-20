1978eb6039262881d732d475b9938b859c31aa23
jacobmakar <jakiemakar+alias@gmail.com>
2024-05-20 16:29:34 -0400

    Refactoring

---
+++ src/trainers/dpo.py
@@ -0,0 +1,161 @@
+import torch 
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from src.utils import evaluate
+import numpy as np 
+import random 
+import wandb
+
+
+def collate(batch):
+    batched_data = {
+        'prompts': [],
+        'winning': [],
+        'losing': []
+    }
+    for item in batch:
+        batched_data['prompts'].append(item['Prompt'])
+        batched_data['winning'].append(item['Winning Continuation'])
+        batched_data['losing'].append(item['Losing Continuation'])
+    return batched_data
+
+
+class DPOTrainer():
+    def __init__(
+            self,
+            model,
+            tokenizer,
+            ref_model,
+            reward_model,
+            reward_tokenizer,
+            optimizer,
+            beta,
+            train_dataset,
+            test_dataset,
+            num_test_batches,
+            prompt_len,
+            sequence_len, 
+            seed,
+            devices,
+
+    ):
+        self.model = model
+        self.ref_model = ref_model 
+        self.reward_model = reward_model
+        self.tokenizer = tokenizer
+        self.reward_tokenizer = reward_tokenizer
+        self.beta = beta 
+        self.train_data = train_dataset
+        self.test_data = test_dataset
+        self.num_test_batches = num_test_batches
+        self.prompt_len = prompt_len
+        self.optimizer = optimizer 
+        self.sequence_len = sequence_len
+        self.seed = seed
+        self.devices = devices
+
+    def loss(self, pi_logps_w, pi_logps_l, ref_logps_w, ref_logps_l, beta):
+        pi_ratio = pi_logps_w - pi_logps_l
+        ref_ratio = ref_logps_w - ref_logps_l
+
+        losses = -F.logsigmoid(beta * (pi_ratio - ref_ratio))
+
+        chosen_rewards = beta * (pi_logps_w - ref_logps_w).detach()
+        rejected_rewards = beta * (pi_logps_l - ref_logps_l).detach()
+       
+        return losses.mean(), chosen_rewards, rejected_rewards
+
+    def compute_log_probs(self, logits, labels, prompt_len):
+        # TODO I have hard-coded a fixed-prompt length to ignore, but this only holds in the imdb exmaple 
+        labels = labels[:, prompt_len + 1:].clone()
+        logits = logits[:, prompt_len:-1, :] 
+        loss_mask = (labels != self.tokenizer.pad_token_id)
+
+        per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
+        return (per_token_logps * loss_mask).sum(-1)
+
+    def compute_kl_divergence(self, pi_logits, ref_logits, prompts_encoded):
+        pi_log_probs = self.compute_log_probs(pi_logits, prompts_encoded['input_ids'], 0)
+        ref_log_probs = self.compute_log_probs(ref_logits, prompts_encoded['input_ids'], 0)
+        diff = (pi_log_probs - ref_log_probs)
+        kl_div = pi_log_probs.exp() * diff
+        return kl_div.sum(-1).mean(), diff.sum(-1).mean()
+            
+    def train(self, batch_size, num_batches):                
+        torch.manual_seed(self.seed)
+        np.random.seed(self.seed)
+        random.seed(self.seed)
+        train_loader = DataLoader(self.train_data, batch_size=batch_size, shuffle=True, collate_fn=collate)
+        self.test_data = self.test_data.select(range(self.num_test_batches * batch_size))
+        test_loader = DataLoader(self.test_data, batch_size=batch_size)
+
+        self.ref_model.eval()
+        self.model.train()
+    
+        count = 0
+        for batch in train_loader: 
+            #### EVAL LOOP ####
+            if count != 0 and count % 5 == 0:
+                avg_kl, avg_reward, policy_text_table = evaluate(test_loader=test_loader, 
+                                                                 sequence_len=self.sequence_len, 
+                                                                 prompt_len=self.prompt_len, 
+                                                                 reward_func=self.reward_func, 
+                                                                 tokenizer=self.tokenizer,
+                                                                 model=self.model, 
+                                                                 ref_model=self.ref_model, 
+                                                                 model_device=self.devices[0])
+                
+                wandb.log({
+                "eval/avg_kl": avg_kl,
+                "eval/avg_reward": avg_reward,
+                "eval/examples": count * batch_size,
+                "policy_samples": policy_text_table,
+                })
+                print(f"Iteration {count}, KL Divergence: {avg_kl:.4f}, Average Reward {avg_reward:.4f}")
+            #### END EVAL LOOP ####
+            if num_batches and count == num_batches:
+                break 
+            count += 1
+            #### TRAIN LOOP ####
+            good_completions = batch['winning']
+            bad_completions = batch['losing']
+
+            good_sequences_encoded = self.tokenizer(good_completions, return_tensors="pt", padding=True).to(self.devices[0])
+            bad_sequences_encoded = self.tokenizer(bad_completions, return_tensors="pt", padding=True).to(self.devices[0])
+
+            
+            with torch.no_grad():
+                ref_logits_good = self.ref_model(**good_sequences_encoded).logits 
+                ref_logits_bad = self.ref_model(**bad_sequences_encoded).logits 
+                ref_log_probs_good = self.compute_log_probs(ref_logits_good, good_sequences_encoded['input_ids'], self.prompt_len)
+                ref_log_probs_bad =  self.compute_log_probs(ref_logits_bad, bad_sequences_encoded['input_ids'], self.prompt_len)
+
+            pi_logits_good = self.model(**good_sequences_encoded).logits 
+            pi_logits_bad = self.model(**bad_sequences_encoded).logits 
+            pi_log_probs_good =  self.compute_log_probs(pi_logits_good, good_sequences_encoded['input_ids'], self.prompt_len)
+            pi_log_probs_bad = self.compute_log_probs(pi_logits_bad, bad_sequences_encoded['input_ids'], self.prompt_len)
+
+            loss, chosen_rewards, rejected_rewards = self.loss(pi_log_probs_good, pi_log_probs_bad, ref_log_probs_good, ref_log_probs_bad, self.beta)
+            mean_chosen_reward, mean_rejected_reward = chosen_rewards.mean(), rejected_rewards.mean()
+            predictions = (chosen_rewards > rejected_rewards).detach().float()
+            correct_predictions = predictions.sum().item()
+            accuracy = correct_predictions / batch_size
+            
+            wandb.log({
+                "train/loss": loss.item(),
+                "train/chosen_reward": mean_chosen_reward,
+                "train/rejected_reward": mean_rejected_reward,
+                "train/accuracy": accuracy,
+                "train/margins": mean_chosen_reward - mean_rejected_reward,
+                "train/iteration": count,
+                "train/examples": len(train_loader) * batch_size + count * batch_size,
+            })
+            
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            print(f"Iteration {count}, Loss: {loss.item():.4f}, Mean Chosen Reward {mean_chosen_reward.item():.4f}, Mean Rejected Reward {mean_rejected_reward.item():.4f}")
+            
+                    
+    def save(self, output_path):
+        torch.save(self.model.state_dict(), output_path)
---
+++ src/utils.py
@@ -0,0 +1,137 @@
+import torch 
+import wandb 
+import numpy as np 
+import pandas as pd
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from nltk import word_tokenize, pos_tag
+from datasets import load_dataset
+from itertools import starmap
+
+class PromptDataset(Dataset):
+    def __init__(self, prompts):
+        self.prompts = prompts
+        self.all = prompts
+
+    def __len__(self):
+        return len(self.prompts)
+
+    def __getitem__(self, idx):
+        return {'text' : self.prompts[idx]}
+
+    def select(self, size):
+        self.prompts = self.all[:size]
+
+class PreferenceDataset(Dataset):
+    def __init__(self, filename):
+        self.dataframe = pd.read_csv(filename)
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, idx):
+        row = self.dataframe.iloc[idx]
+        return {
+            'Prompt': row['Prompt'],
+            'Winning Continuation':  row['Winning Continuation'],
+            'Losing Continuation':  row['Losing Continuation']
+        }
+
+bow_words = [
+    'data', 'hope', 'information', 'provide', 'example', 'your', 'however', 'first', 'have', 'help', 
+    'additionally', 'important', 'include', 'finally', 'following', 'happy', 'code', 'two', 'create', 'question', 'possible', 'understand', 'generate', 'contains', 
+    'appropriate', 'best', 'respectful', 'ensure', 'experience', 'safe'
+]
+
+
+def score_words(prompt, response):
+    '''Returns number of words from `bow_words` in response'''
+    words = word_tokenize(response)
+    sco = 0
+    for t in bow_words: 
+        if t in words: 
+            sco += 1
+    return float(sco)
+
+def score_negative(prompt, response):
+    '''Returns number of words from `bow_words` in response'''
+    neg_words = word_tokenize(prompt)
+
+
+    words = word_tokenize(response)
+    pos_sco = 0
+    neg_sco = 0
+
+    for t in bow_words: 
+        if t in neg_words:
+            neg_sco += 1
+        elif t in words: 
+            pos_sco += 1 
+    return float(pos_sco - neg_sco)
+
+def score_nouns(prompt, response, pstr="NN"):     
+    '''Returns number of nouns in response'''        
+    tokens = word_tokenize(response)
+    tagged = pos_tag(tokens)
+    
+    return len([word for word, pos in tagged if pos.startswith(pstr)])
+
+def evaluate(test_loader, sequence_len, prompt_len, reward_func, tokenizer, model, ref_model, model_device):
+    model.eval()
+    with torch.no_grad():
+        total_kl, total_reward = 0, 0
+        for eval_batch in test_loader:
+            # KL Calculation
+            prompts_encoded = tokenizer(eval_batch['text'], return_tensors="pt", max_length=128, padding=True, truncation=True).to(model_device)
+            ref_logits = ref_model(**prompts_encoded).logits
+            pi_logits = model(**prompts_encoded).logits 
+            pi_log_probs = F.log_softmax(pi_logits, dim=-1)
+            ref_probs = F.softmax(ref_logits, dim=-1)   
+            kl_div = F.kl_div(pi_log_probs, ref_probs, reduction='batchmean', log_target=False)
+            total_kl += kl_div.item()
+            
+            # Reward Calculation
+            prompts_encoded = tokenizer(eval_batch['text'], return_tensors="pt", max_length=prompt_len, padding=True, truncation=True).to(model_device)
+            prompts_encoded = {k: v.to(model_device) for k, v in prompts_encoded.items()} # devices[0]
+            output_sequences = model.generate(
+                input_ids=prompts_encoded['input_ids'],
+                max_length=sequence_len,
+                num_return_sequences=1,
+                do_sample=True, 
+                pad_token_id=tokenizer.eos_token_id,
+            )
+            completions = [tokenizer.decode(output_sequence, skip_special_tokens=True) for output_sequence in output_sequences[:, prompt_len:-1, :]]
+
+            avg_rewards = np.mean(list(starmap(reward_func, zip(prompts, completions))))
+            total_reward += avg_rewards
+
+        prompts = [tokenizer.decode(prompt) for prompt in prompts_encoded['input_ids']]
+        policy_text_table = wandb.Table(columns=["prompt", "sample"])
+        for prompt, sample in zip(prompts, completions):
+            policy_text_table.add_data(prompt, sample)   
+
+        # Explicitly delete variables to help with memory usage
+        del prompts_encoded, output_sequences, ref_logits, pi_logits, ref_probs, pi_log_probs, kl_div
+        avg_kl = total_kl / len(test_loader)
+        avg_reward = total_reward /  len(test_loader)
+        torch.cuda.empty_cache()     
+        return avg_kl, avg_reward, policy_text_table
+
+def preprocess_and_save_ultrachat():
+    dataset = load_dataset("stingning/ultrachat", split="train")
+    data = dataset['data']
+    train_data = list(map(lambda i: i[0], data[1013:33013]))
+    test_data = list(map(lambda i: i[0], data[13:173]))
+
+    # Convert to DataFrame and save as CSV
+    train_df = pd.DataFrame(train_data, columns=['text'])
+    test_df = pd.DataFrame(test_data, columns=['text'])
+    train_df.to_csv('train_ultrachat.csv', index=False)
+    test_df.to_csv('test_ultrachat.csv', index=False)
+
+    return train_data, test_data
+
+def load_ultrachat():
+    train_df = pd.read_csv('train_ultrachat.csv')
+    test_df = pd.read_csv('test_ultrachat.csv')
+    return train_df['text'].tolist(), test_df['text'].tolist()
