dd81ae9c0531e70de8d0fda1f914e4cbd96c888a
jacobmakar <jakiemakar+alias@gmail.com>
2024-05-21 13:41:40 -0400

    check prompt len type

--- src/trainers/train_utils.py
+++ src/trainers/train_utils.py
@@ -1,7 +1,8 @@
-def compute_log_probs(self, logits, labels, prompt_len, pad_token_id):
-    labels = labels[:, prompt_len + 1:].clone()
-    logits = logits[:, prompt_len:-1, :] 
-    loss_mask = (labels != pad_token_id)
+def compute_log_probs(logits, labels, prompt_lens, pad_token_id):
+    if type(prompt_lens) == int:
+        labels = labels[:, prompt_len + 1:].clone()
+        logits = logits[:, prompt_len:-1, :] 
+        loss_mask = (labels != pad_token_id)
 
-    per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
-    return (per_token_logps * loss_mask).sum(-1)
+        per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
+        return (per_token_logps * loss_mask).sum(-1)
