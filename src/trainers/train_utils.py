dfb6f3d408663af555e4aa9a0ffae08c54e4d315
jacobmakar <jakiemakar+alias@gmail.com>
2024-05-21 12:39:10 -0400

    pad token as arg

--- src/trainers/train_utils.py
+++ src/trainers/train_utils.py
@@ -1,7 +1,7 @@
-def compute_log_probs(self, logits, labels, prompt_len):
+def compute_log_probs(self, logits, labels, prompt_len, pad_token_id):
     labels = labels[:, prompt_len + 1:].clone()
     logits = logits[:, prompt_len:-1, :] 
-    loss_mask = (labels != self.tokenizer.pad_token_id)
+    loss_mask = (labels != pad_token_id)
 
     per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
     return (per_token_logps * loss_mask).sum(-1)
