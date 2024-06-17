ffff9ddcb1dd46c42fd036ff8ba6070833142216
jacobmakar <jakiemakar+alias@gmail.com>
2024-06-17 12:53:58 -0400

    refactoring concat sequences

--- src/trainers/train_utils.py
+++ src/trainers/train_utils.py
@@ -1,9 +1,14 @@
 import torch
-def compute_log_probs(logits, labels, prompt_lens, pad_token_id):
-    if type(prompt_lens) == int:
-        labels = labels[:, prompt_lens + 1:].clone()
-        logits = logits[:, prompt_lens:-1, :] 
-        loss_mask = (labels != pad_token_id)
+def compute_log_probs(logits, labels, prompt_len, pad_token_id):
+    labels = labels[:, prompt_len + 1:].clone()
+    logits = logits[:, prompt_len:-1, :] 
+    loss_mask = (labels != pad_token_id)
 
-        per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
-        return (per_token_logps * loss_mask).sum(-1)
+    per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
+    return (per_token_logps * loss_mask).sum(-1)
+
+def concat_sequences(seq1, seq2, seq_len):
+        truncate_len = min(seq1['input_ids'].size()[1], seq2['input_ids'].size()[1], seq_len)
+        input_ids = torch.cat((seq1['input_ids'][:, :truncate_len], seq2['input_ids'][:, :truncate_len]), dim=0)
+        attention_mask = torch.cat((seq1['attention_mask'][:, :truncate_len], seq2['attention_mask'][:, :truncate_len]), dim=0)
+        return input_ids, attention_mask
